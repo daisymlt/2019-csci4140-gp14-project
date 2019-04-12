5963ae2e78e16302a5e4d311e3979466c23a7a9d
Daisy Mang <longting0709@yahoo.com.hk>
2019-04-13 02:43:02 +0800

    music player can now play, pause and adjust volume

--- musicplayer/js/sidebar.js
+++ musicplayer/js/sidebar.js
@@ -1,7 +1,7 @@
 document.write('<html>');
 document.write('<head>');
-document.write('<meta charset="UTF-8"><title>Hello World!</title><link rel="stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.min.css"><link rel="stylesheet" type="text/css" href="../css/main_style.css">');
+document.write('<meta charset="UTF-8"><title>Hello World!</title><link rel="stylesheet" type="text/css" href="../node_modules/bootstrap/dist/css/bootstrap.min.css"><link rel="stylesheet" type="text/css" href="../css/main_style.css">');
 document.write('</head><body><div class="wrapper"><!-- Sidebar --><nav id="sidebar"><div class="sidebar-header"><h3>CSCI4140 project</h3></div>');
 document.write('<ul class="list-unstyled components"><!-- <p>Dummy Heading</p> --><li><a href="index.html">Home</a></li><li>');
-document.write('<a href="music.html">Player</a></li><li><a href="http://www.youtube.com">Youtube (wont go back)</a></li><li><a href="fav.html">Favourite</a>');
+document.write('<a href="music.html">Music Player</a></li><li><a href="http://www.youtube.com">Youtube (wont go back)</a></li><li><a href="fav.html">Favourite</a>');
 document.write('</li><li><a href="about.html">About</a></li></ul></nav><div id="content"><nav class="navbar navbar-expand-lg navbar-light bg-light"><div class="container-fluid"></div>');
--- musicplayer/main.js
+++ musicplayer/main.js
@@ -1,20 +1,20 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win;
+let win
 
 function createWindow () {
   // 建立瀏覽器視窗。
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({ width: 800, height: 600 })
 
   // 並載入應用程式的 index.html。
-  win.loadFile('html/index.html');
+  win.loadFile('html/index.html')
 
   // Open the DevTools.
   //win.webContents.openDevTools()
 
-  console.log('testing');
+  console.log('testing')
 
   win.webContents.on('dom-ready', () => {
   // 視窗關閉時會觸發。
@@ -29,7 +29,7 @@ function createWindow () {
 // 當 Electron 完成初始化，並且準備好建立瀏覽器視窗時
 // 會呼叫這的方法
 // 有些 API 只能在這個事件發生後才能用。
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 // 在所有視窗都關閉時結束程式。
 app.on('window-all-closed', () => {
@@ -38,7 +38,7 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   // 在 macOS 中，一般會在使用者按了 Dock 圖示
@@ -47,7 +47,7 @@ app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
-});
+})
 
 // 你可以在這個檔案中繼續寫應用程式主程序要執行的程式碼。 
 // 你也可以將它們放在別的檔案裡，再由這裡 require 進來。
